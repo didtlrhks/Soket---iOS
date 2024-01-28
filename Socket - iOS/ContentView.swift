d0bba80798003270c5f204e5cf7febb7161e1a28
sikwan <yysskk99999@naver.com>
2024-01-28 18:24:32 +0900

    pod deintegrate

--- Socket - iOS/ContentView.swift
+++ Socket - iOS/ContentView.swift
@@ -8,41 +8,29 @@
 import SwiftUI
 import Foundation
 
+
 struct ContentView: View {
-    @StateObject var webSocketManager = WebSocketManager()
+    @StateObject var viewModel = RabbitMQViewModel()
 
     var body: some View {
         VStack {
-            // 수신된 메시지를 표시하는 뷰
-            List(webSocketManager.receivedMessages, id: \.self) { message in
+            List(viewModel.receivedMessages, id: \.self) { message in
                 Text(message)
-                Text("메세지 보냇음")
             }
-
-            // 메시지를 보내는 버튼
             Button("Send Message") {
-                // 'Hello, World!' 메시지를 서버로 전송합니다.
-                webSocketManager.sendMessage("양시관")
-                    print("메세지 액션 시작함")
-                
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(5)
+                         // 이 부분에서 메시지를 보냅니다.
+                         viewModel.sendMessage("Hello RabbitMQ")
+                     }
         }
         .onAppear {
-            // 뷰가 나타날 때 웹소켓 연결을 시작합니다.
-            webSocketManager.connect()
-            print("소켓연결 시작했음")
+            viewModel.connectToRabbitMQ()
         }
         .onDisappear {
-            // 뷰가 사라질 때 웹소켓 연결을 종료합니다.
-            webSocketManager.disconnect()
-            print("소켓연결 끝났음")
+            viewModel.disconnect()
         }
     }
 }
+
 
 
 #Preview {
--- Socket - iOS/WebSocketManager.swift
+++ Socket - iOS/WebSocketManager.swift
@@ -6,73 +6,41 @@
 //
 
 import Foundation
-import Foundation
 import SwiftUI
+import RMQClient
+import UIKit
 
 
-// 웹 소켓 연결을 관리하는 클래스
-class WebSocketManager : ObservableObject{
-    private var webSocketTask: URLSessionWebSocketTask?
+class RabbitMQViewModel: ObservableObject {
     @Published var receivedMessages: [String] = []
-    
-    // 웹 소켓 연결을 시작하는 함수
-    func connect() {
-        let url = URL(string: "ws://112.219.138.170:8080")! // 실제 웹소켓 서버 주소로 바꿔야 합니다.
-        var request = URLRequest(url: url)
-        let userName = "neo"
-        let password = "01579"
-            
+    private var connection: RMQConnection?
+    private var channel: RMQChannel?
 
-        // 사용자 이름과 비밀번호를 Base64 인코딩하여 HTTP 기본 인증 헤더를 생성
-        let credentials = "\(userName):\(password)"
-        let credentialsData = Data(credentials.utf8)
-        let base64Credentials = credentialsData.base64EncodedString(options: [])
-        request.addValue("Basic \(base64Credentials)", forHTTPHeaderField: "Authorization")
+    func connectToRabbitMQ() {
+        let factory = RMQConnectionFactory()
+        factory.hostName = "112.219.138.170"
+        factory.userName = "neo"
+        factory.password = "01579"
 
-        // 웹소켓 연결을 위한 URLSessionWebSocketTask 생성 및 시작
-        webSocketTask = URLSession.shared.webSocketTask(with: request)
-        webSocketTask?.resume()
+        self.connection = factory.newConnection()
+        self.channel = connection?.createChannel()
 
-        // 메시지 수신을 위한 리스너 시작
-        listenForMessages()
-    }
-    // 메시지를 보내는 함수
-    func sendMessage(_ message: String) {
-        let message = URLSessionWebSocketTask.Message.string(message)
-        webSocketTask?.send(message) { error in
-            if let error = error {
-                print("Error sending message: \(error)")
+        let queueName = channel?.queueDeclare().queueName
+        channel?.queueBind(queueName, exchange: "neotest", routingKey: "")
+
+        let consumer = RMQConsumer(channel: channel, queueName: queueName, options: .noLocal)
+        consumer.onReceived { message in
+            let body = message.body
+            let text = String(data: body, encoding: .utf8) ?? ""
+            DispatchQueue.main.async {
+                self.receivedMessages.append(text)
             }
         }
+
+        channel?.basicConsume(queueName, consumer: consumer)
     }
-    
-    // 메시지를 수신하는 함수
-    private func listenForMessages() {
-        webSocketTask?.receive { [weak self] result in
-            switch result {
-            case .failure(let error):
-                print("Error receiving message: \(error)")
-                
-                
-            case .success(.string(let message)):
-                DispatchQueue.main.async {
-                                 self?.receivedMessages.append(message)
-                             }
-                self?.listenForMessages() 
-                
-                
-                // 다음 메시지를 계속 수신하기 위해 재귀적으로 호출
-            case .success(.data(let data)):
-                print("Received data: \(data)")
-                self?.listenForMessages() // 다음 메시지를 계속 수신하기 위해 재귀적으로 호출
-            default:
-                break
-            }
-        }
-    }
-    
-    // 웹 소켓 연결을 종료하는 함수
+
     func disconnect() {
-        webSocketTask?.cancel(with: .goingAway, reason: nil)
+        connection?.close()
     }
 }
