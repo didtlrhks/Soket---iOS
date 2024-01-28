b0b7e96f55a80184d97631e127961d0651739c86
sikwan <yysskk99999@naver.com>
2024-01-28 15:24:31 +0900

    init

---
+++ Socket - iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Socket - iOS
+//
+//  Created by 양시관 on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
